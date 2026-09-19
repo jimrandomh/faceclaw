1e1c3f799dda3b1938a6b2584a17108bfcd7de10
James Babcock <jimrandomh@gmail.com>
2026-09-18 19:35:50 -0700

    Introduce Kotlin

--- app/app.ts
+++ app/app.ts
@@ -8,6 +8,11 @@ import { Application } from '@nativescript/core'
 import { registerShareIntentHandler } from './native/share-intents'
 import { installNativeUserAgent } from './util/http'
 import { registerPhoneRotation } from './native/phone-rotation'
+import { runKotlinBridgeSmokeTest } from './native/kotlin-bridge'
+
+declare const __DEV__: boolean;
+
+if (__DEV__) runKotlinBridgeSmokeTest()
 
 installNativeUserAgent()
 registerShareIntentHandler()
